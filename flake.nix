e9d296a5678d945d6b5ebe4d7f5e72f18ef48467
Hlib Hraif <hlib.hraif@protonmail.com>
2026-04-09 17:27:15 +0200

    Switch to builtins.fetchurl, support external inputs

--- flake.nix
+++ flake.nix
@@ -6,71 +6,106 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
     let
       baseUrl = "https://paddle-model-ecology.bj.bcebos.com/paddlex/official_inference_model/paddle3.0.0";
 
       models = {
         cyrillic-pp-ocrv5-mobile-det = {
-          fetchName = "cyrillic_PP-OCRv5_mobile_rec";
-          sha256 = "sha256-7wxm8Z6+aEna+ap0heklPSqNhR4WXiCL8IumimIK5kU=";
+          src = builtins.fetchurl {
+            url = "${baseUrl}/cyrillic_PP-OCRv5_mobile_rec_infer.tar";
+            sha256 = "ef0c66f19ebe6849daf9aa7485e9253d2a8d851e165e208bf08ba68a620ae645";
+          };
           outputHash = "sha256-Pl90W8sOFGf935TGJcXt2YLAWcri6+VfDkf5ZXvmr28=";
         };
         latin-pp-ocrv5-mobile-rec = {
-          fetchName = "latin_PP-OCRv5_mobile_rec";
-          sha256 = "sha256-sjEFpqHqOOMql8Wg3cfoqbv1QdjkdCHiyZ6cyr4pUJw=";
+          src = builtins.fetchurl {
+            url = "${baseUrl}/latin_PP-OCRv5_mobile_rec_infer.tar";
+            sha256 = "b23105a6a1ea38e32a97c5a0ddc7e8a9bbf541d8e47421e2c99e9ccabe29509c";
+          };
           outputHash = "sha256-KFyDLqf+V0amt2NN/wpUms9mw9hGM+eYHRII3XzY1rk=";
         };
         en-pp-ocrv5-mobile-det = {
-          fetchName = "en_PP-OCRv5_mobile_rec";
-          sha256 = "sha256-5ZW0zy/60Z+7WmG6NF1jk5V3o6uHF7blmVZCWQyRAbQ=";
+          src = builtins.fetchurl {
+            url = "${baseUrl}/en_PP-OCRv5_mobile_rec_infer.tar";
+            sha256 = "e595b4cf2ffad19fbb5a61ba345d63939577a3ab8717b6e5995642590c9101b4";
+          };
           outputHash = "sha256-senkAmebyoicQ0lw9c9qNuWbTMiA992mzz734G9Hsr4=";
         };
         eslav-pp-ocrv5-mobile-rec = {
-          fetchName = "eslav_PP-OCRv5_mobile_rec";
-          sha256 = "sha256-ufcNoMorvE1Mt7pAai0CMGEXhDfWqTDwfIyhjGxZGDk=";
+          src = builtins.fetchurl {
+            url = "${baseUrl}/eslav_PP-OCRv5_mobile_rec_infer.tar";
+            sha256 = "b9f70da0ca2bbc4d4cb7ba406a2d023061178437d6a930f07c8ca18c6c591839";
+          };
           outputHash = "sha256-JVC3YVSOc5ClRrLrD7Q4BcrYZzeLtFatkVmYTZ8MNWI=";
         };
         pp-lcnet-x0-25-textline-ori = {
-          fetchName = "PP-LCNet_x0_25_textline_ori";
-          sha256 = "sha256-Jo2a6mFGHD1KWjJ1LluSDory7goAI2L2ulzTljj6LDo=";
+          src = builtins.fetchurl {
+            url = "${baseUrl}/PP-LCNet_x0_25_textline_ori_infer.tar";
+            sha256 = "268d9aea61461c3d4a5a32752e5b920e8af2ee0a002362f6ba5cd39638fa2c3a";
+          };
           outputHash = "sha256-Dg5cvewZ817b6r1GQhyhv+KJIxHVdSxZVYVfFvZkxtM=";
-          inputShape = { x = [1 3 80 160]; };
+          inputShape = {
+            x = [
+              1
+              3
+              80
+              160
+            ];
+          };
         };
         pp-lcnet-x1-0-doc-ori = {
-          fetchName = "PP-LCNet_x1_0_doc_ori";
-          sha256 = "sha256-KCM331xB98342s1az3H939wQIYOZ9LMYRjwX9OrpbJc=";
+          src = builtins.fetchurl {
+            url = "${baseUrl}/PP-LCNet_x1_0_doc_ori_infer.tar";
+            sha256 = "282337df5c41f7cdf8dacd5acf71fddfdc10218399f4b318463c17f4eae96c97";
+          };
           outputHash = "sha256-8fIZLzE8MQDXAVHeSeGB9FgSjV5vPhnLjmGigie4Itw=";
         };
         pp-ocrv5-mobile-det = {
-          fetchName = "PP-OCRv5_mobile_det";
-          sha256 = "sha256-UERuXQGsKnPVMZyJUTKB9leEFMiIxgL5rxP5P+7//Fg=";
+          src = builtins.fetchurl {
+            url = "${baseUrl}/PP-OCRv5_mobile_det_infer.tar";
+            sha256 = "50446e5d01ac2a73d5319c89513281f6578414c888c602f9af13f93feefffc58";
+          };
           outputHash = "sha256-8laQoLO5ZER+1T1m7TxK2oQv8Jvb2unWoFeUR2/OsWo=";
         };
         pp-ocrv5-mobile-rec = {
-          fetchName = "PP-OCRv5_mobile_rec";
-          sha256 = "sha256-VmuVErNONKnw21TYe1H6Wgue0s8at+SXKMwLi1pk9BQ=";
+          src = builtins.fetchurl {
+            url = "${baseUrl}/PP-OCRv5_mobile_rec_infer.tar";
+            sha256 = "566b9512b34e34a9f0db54d87b51fa5a0b9ed2cf1ab7e49728cc0b8b5a64f414";
+          };
           outputHash = "sha256-IJvx9G3F4Pz1Ye+zJ90pX69Z3SpDDPpyqMDfQ5V5CSk=";
         };
         pp-ocrv5-server-det = {
-          fetchName = "PP-OCRv5_server_det";
-          sha256 = "sha256-IqM+C6aiFCXqQZLaA79DlcmgxnkCvZJLcyj8hZBzBF0=";
+          src = builtins.fetchurl {
+            url = "${baseUrl}/PP-OCRv5_server_det_infer.tar";
+            sha256 = "22a33e0ba6a21425ea4192da03bf4395c9a0c67902bd924b7328fc859073045d";
+          };
           outputHash = "sha256-kXOaDvXzPOxEysnGsvckoHyys68nYlFM5X3unZA94H4=";
         };
         pp-ocrv5-server-rec = {
-          fetchName = "PP-OCRv5_server_rec";
-          sha256 = "sha256-2Zvi/9NIlDq1KHYXkWi+T7WxT18IEvKuTHbYnsLqdQo=";
+          src = builtins.fetchurl {
+            url = "${baseUrl}/PP-OCRv5_server_rec_infer.tar";
+            sha256 = "d99be2ffd348943ab52876179168be4fb5b14f5f0812f2ae4c76d89ec2ea750a";
+          };
           outputHash = "sha256-GruBSgCVA1Ww81IR3Ty2IZFFBwG2A8egXmc0Q77YvUM=";
         };
         uvdoc = {
-          fetchName = "UVDoc";
-          sha256 = "sha256-Fdeca8v3OLfhMu7f9u1Xx+g0xzy8FAof/4/Yq5wqNbk=";
+          src = builtins.fetchurl {
+            url = "${baseUrl}/UVDoc_infer.tar";
+            sha256 = "15d79c6bcbf738b7e132eedff6ed57c7e834c73cbc140a1fff8fd8ab9c2a35b9";
+          };
           outputHash = "sha256-jJR909VvOX8eAyc8q5/Bc5KavFRYZRad8V243abG5AI==";
           patches = [ ./patches/0001-uvdoc-rename-img-to-image.patch ];
         };
       };
 
-      mkModels = pkgs:
+      mkModels =
+        pkgs: modelDefs:
         let
           lib = pkgs.lib;
           paddle2onnx = pkgs.python312Packages.callPackage ./modules/paddle2onnx.nix {
@@ -78,84 +113,82 @@
           };
           hasCuda = pkgs.config.cudaSupport or false;
 
-          mkOnnxModel = name: {
-            fetchName,
-            sha256,
-            outputHash,
-            modelFilename ? "inference.json",
-            paramsFilename ? "inference.pdiparams",
-            inputShape ? null,
-            patches ? [],
-          }:
-            let
-              tarName = "${fetchName}_infer";
-              drv = pkgs.stdenvNoCC.mkDerivation {
-                pname = name;
-                version = "3.0.0";
+          mkOnnxModel =
+            name:
+            {
+              src,
+              outputHash,
+              modelFilename ? "inference.json",
+              paramsFilename ? "inference.pdiparams",
+              inputShape ? null,
+              patches ? [ ],
+            }:
+            pkgs.stdenvNoCC.mkDerivation {
+              pname = name;
+              version = "3.0.0";
 
-                src = pkgs.fetchurl {
-                  url = "${baseUrl}/${tarName}.tar";
-                  inherit sha256;
-                };
+              inherit src patches;
 
-                nativeBuildInputs = [
-                  pkgs.ccache
-                  paddle2onnx
-                ] ++ lib.optionals pkgs.stdenv.hostPlatform.isDarwin [
-                  pkgs.darwin.system_cmds
-                ] ++ lib.optionals pkgs.stdenv.hostPlatform.isLinux [
-                  pkgs.procps
-                ];
+              nativeBuildInputs = [
+                pkgs.ccache
+                paddle2onnx
+              ]
+              ++ lib.optionals pkgs.stdenv.hostPlatform.isDarwin [
+                pkgs.darwin.system_cmds
+              ]
+              ++ lib.optionals pkgs.stdenv.hostPlatform.isLinux [
+                pkgs.procps
+              ];
 
-                env = lib.optionalAttrs hasCuda {
-                  LD_LIBRARY_PATH = "${pkgs.cudaPackages.cudatoolkit}/lib";
-                  CUDA_PATH = "${pkgs.cudaPackages.cudatoolkit}";
-                };
-
-                sourceRoot = tarName;
-
-                inherit patches;
-
-                buildPhase = ''
-                  paddle2onnx \
-                    --model_dir . \
-                    --model_filename "${modelFilename}" \
-                    --params_filename "${paramsFilename}" \
-                    --save_file model.onnx \
-                    --optimize_tool None
-                '' + lib.optionalString (inputShape != null) ''
-                  python3 -m paddle2onnx.optimize \
-                    --input_model model.onnx \
-                    --output_model model.onnx \
-                    --input_shape_dict '${builtins.toJSON inputShape}'
-                '';
-
-                installPhase = ''
-                  mkdir -p $out
-                  cp model.onnx $out/model.onnx
-                  cp inference.yml $out/config.yml
-                '';
-
-                inherit outputHash;
-                outputHashAlgo = "sha256";
-                outputHashMode = "recursive";
-
-                meta = {
-                  description = "ONNX export of ${fetchName}";
-                };
+              env = lib.optionalAttrs hasCuda {
+                LD_LIBRARY_PATH = "${pkgs.cudaPackages.cudatoolkit}/lib";
+                CUDA_PATH = "${pkgs.cudaPackages.cudatoolkit}";
               };
-            in drv;
+
+              buildPhase = ''
+                paddle2onnx \
+                  --model_dir . \
+                  --model_filename "${modelFilename}" \
+                  --params_filename "${paramsFilename}" \
+                  --save_file model.onnx \
+                  --optimize_tool None
+              ''
+              + lib.optionalString (inputShape != null) ''
+                python3 -m paddle2onnx.optimize \
+                  --input_model model.onnx \
+                  --output_model model.onnx \
+                  --input_shape_dict '${builtins.toJSON inputShape}'
+              '';
+
+              installPhase = ''
+                mkdir -p $out
+                cp model.onnx $out/model.onnx
+                cp inference.yml $out/config.yml
+              '';
+
+              inherit outputHash;
+              outputHashAlgo = "sha256";
+              outputHashMode = "recursive";
+
+              meta = {
+                description = "ONNX export of ${name}";
+              };
+            };
         in
-        lib.mapAttrs mkOnnxModel models;
+        lib.mapAttrs mkOnnxModel modelDefs;
 
     in
     {
-      lib.mkModels = mkModels;
+      lib = {
+        inherit models mkModels;
+      };
 
-    } // flake-utils.lib.eachDefaultSystem (system:
+    }
+    // flake-utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = import nixpkgs { inherit system; };
-        modelDerivations = mkModels pkgs;
+        modelDerivations = mkModels pkgs models;
       in
       {
         packages = modelDerivations;
